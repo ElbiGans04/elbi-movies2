f07c6d14c2f79cc70f8c498a83fc4937412bbcf2
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-09-29 18:58:59 +0700

    feat: create loading component & error component
    create and use those compoent in index and detail views

---
+++ src/components/error.tsx
@@ -0,0 +1,8 @@
+export default function ErrorComponent() {
+  return (
+    <div className="text-white space-y-[10px]">
+      <p className="text-2xl hover:underline font-bold">Sorry</p>
+      <p>An error occurred in the application</p>
+    </div>
+  );
+}
--- src/views/detail.tsx
+++ src/views/detail.tsx
@@ -5,9 +5,11 @@ import numeral from "numeral";
 import dayjs from "dayjs";
 import Button from "../components/button";
 import Footer from "../components/footer";
+import LoadingComponent from "../components/loading";
+import ErrorComponent from "../components/error";
 export default function MovieDetailComponent() {
   const { id } = useParams();
-  const { data } = useSWR(
+  const { data, isLoading, error } = useSWR(
     `${process.env.REACT_APP_MOVIE_DETAIL}${id}`,
     fetcher
   );
@@ -24,151 +26,166 @@ export default function MovieDetailComponent() {
       className={`bg-brand-900 min-h-screen pb-[50px] w-full h-full flex flex-col items-center justify-start`}
     >
       <div className="w-full h-[350px] bg-slate-900 shadow-xl hidden lg:block">
-        <img
-          src={`${process.env.REACT_APP_IMAGE_URL}${data?.backdrop_path}`}
-          alt="background"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="xl:max-w-5xl lg:max-w-3xl px-[x50px] w-full h-full lg:-mt-[250px] px-[20px] py-[16px] md:p-[20px] flex flex-col md:flex-row rounded overflow-hidden md:space-x-[16px] bg-brand-800 shadow-xl">
-        <div className="w-[300px] h-[400px] overflow-hidden shadow-xl shrink-[0]">
+        {!error && !isLoading && data && (
           <img
-            src={`${process.env.REACT_APP_IMAGE_URL}${data?.poster_path}`}
+            src={`${process.env.REACT_APP_IMAGE_URL}${data?.backdrop_path}`}
             alt="background"
             className="w-full h-full object-cover"
           />
-        </div>
+        )}
+      </div>
+      <div className="xl:max-w-5xl lg:max-w-3xl px-[x50px] w-full h-full lg:-mt-[250px] px-[20px] py-[16px] md:p-[20px] flex flex-col md:flex-row rounded overflow-hidden md:space-x-[16px] bg-brand-800 shadow-xl">
+        {!isLoading && error && <ErrorComponent />}
+        {isLoading && !error && <LoadingComponent />}
 
-        <div className="w-full h-full space-y-[30px] lg:px-[16px] mt-[16px] md:mt-[0]">
-          <div className="space-y-[10px] text-white h-full">
-            <h1 className="xl:text-7xl text-3xl md:text-4xl font-bold">
-              {data?.title}
-            </h1>
-            <p className="xl:text-2xl md:text-xl text-lg">
-              {data?.genres
-                .map(({ name }: { name: string }) => name)
-                .join(", ")}
-            </p>
-            <table className="!mt-[20px]">
-              <tbody>
-                <tr>
-                  <td>Release Date</td>
-                  <td className="pl-5">
-                    : {dayjs(data?.release_date).format("MMM, DD YYYY")}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Native Language</td>
-                  <td className="pl-5">: {data?.original_language || "-"}</td>
-                </tr>
-                <tr>
-                  <td>Duration</td>
-                  <td className="pl-5">: {data?.runtime || 0} Minutes</td>
-                </tr>
-                <tr>
-                  <td>Status</td>
-                  <td className="pl-5">: {data?.status || "-"}</td>
-                </tr>
-                <tr>
-                  <td>IMDB ID</td>
-                  <td className="pl-5">: {data?.imdb_id || "-"}</td>
-                </tr>
-                <tr>
-                  <td>Tag Line</td>
-                  <td className="pl-5">: {data?.tagline || "-"}</td>
-                </tr>
-              </tbody>
-            </table>
+        {!isLoading && !error && (
+          <>
+            <div className="w-[300px] h-[400px] overflow-hidden shadow-xl shrink-[0]">
+              <img
+                src={`${process.env.REACT_APP_IMAGE_URL}${data?.poster_path}`}
+                alt="background"
+                className="w-full h-full object-cover"
+              />
+            </div>
 
-            <p className="!mt-[40px]">{data?.overview || "-"}</p>
-
-            <div className="space-y-[30px] !mt-[50px]">
-              <div className="space-y-[20px]">
-                <h1 className="text-4xl font-bold">Production Information</h1>
-                <div className="h-[1px] w-full bg-[white]" />
-                <table>
-                  <tbody className="text-white">
+            <div className="w-full h-full space-y-[30px] lg:px-[16px] mt-[16px] md:mt-[0]">
+              <div className="space-y-[10px] text-white h-full">
+                <h1 className="xl:text-7xl text-3xl md:text-4xl font-bold">
+                  {data?.title}
+                </h1>
+                <p className="xl:text-2xl md:text-xl text-lg">
+                  {data?.genres
+                    .map(({ name }: { name: string }) => name)
+                    .join(", ")}
+                </p>
+                <table className="!mt-[20px]">
+                  <tbody>
                     <tr>
-                      <td>Budget</td>
+                      <td>Release Date</td>
                       <td className="pl-5">
-                        : ${numeral(data?.budget || 0).format("0,0")}
+                        : {dayjs(data?.release_date).format("MMM, DD YYYY")}
                       </td>
                     </tr>
                     <tr>
-                      <td>Revenue</td>
+                      <td>Native Language</td>
                       <td className="pl-5">
-                        : ${numeral(data?.revenue || 0).format("0,0")}
+                        : {data?.original_language || "-"}
                       </td>
                     </tr>
                     <tr>
-                      <td>Production Company</td>
-                      <td className="pl-5">
-                        :{" "}
-                        {data?.production_companies
-                          ?.map(({ name }: { name: string }) => name)
-                          .join(", ") || "-"}
-                      </td>
+                      <td>Duration</td>
+                      <td className="pl-5">: {data?.runtime || 0} Minutes</td>
+                    </tr>
+                    <tr>
+                      <td>Status</td>
+                      <td className="pl-5">: {data?.status || "-"}</td>
+                    </tr>
+                    <tr>
+                      <td>IMDB ID</td>
+                      <td className="pl-5">: {data?.imdb_id || "-"}</td>
+                    </tr>
+                    <tr>
+                      <td>Tag Line</td>
+                      <td className="pl-5">: {data?.tagline || "-"}</td>
                     </tr>
                   </tbody>
                 </table>
-              </div>
-            </div>
 
-            <div className="space-y-[30px] !mt-[50px]">
-              <div className="space-y-[20px]">
-                <h1 className="text-4xl font-bold">Aditional Information</h1>
-                <div className="h-[1px] w-full bg-[white]" />
-                <table>
-                  <tbody className="text-white">
-                    <tr>
-                      <td>Rating</td>
-                      <td className="pl-5">
-                        : {data?.vote_average || "-"} / 10 from a total of{" "}
-                        {data?.vote_count || "-"} assessments
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Website Home Page</td>
-                      <td className="pl-5">: {data?.homepage || "-"}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
+                <p className="!mt-[40px]">{data?.overview || "-"}</p>
 
-            <div className="space-y-[30px] !mt-[50px]">
-              <div className="space-y-[20px]">
-                <h1 className="text-4xl font-bold">Images</h1>
-                <div className="h-[1px] w-full bg-[white]" />
-                <div className="flex flex-col items-start justify-start">
-                  {dataImages?.backdrops
-                    ?.filter(
-                      ({ iso_639_1 }: { iso_639_1: string }) => !iso_639_1
-                    )
-                    .map(({ file_path }: { file_path: string }) => (
-                      <div>
-                        <img
-                          src={`${process.env.REACT_APP_IMAGE_URL}${file_path}`}
-                          alt="images"
-                        />
-                      </div>
-                    ))}
+                <div className="space-y-[30px] !mt-[50px]">
+                  <div className="space-y-[20px]">
+                    <h1 className="text-4xl font-bold">
+                      Production Information
+                    </h1>
+                    <div className="h-[1px] w-full bg-[white]" />
+                    <table>
+                      <tbody className="text-white">
+                        <tr>
+                          <td>Budget</td>
+                          <td className="pl-5">
+                            : ${numeral(data?.budget || 0).format("0,0")}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Revenue</td>
+                          <td className="pl-5">
+                            : ${numeral(data?.revenue || 0).format("0,0")}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Production Company</td>
+                          <td className="pl-5">
+                            :{" "}
+                            {data?.production_companies
+                              ?.map(({ name }: { name: string }) => name)
+                              .join(", ") || "-"}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+
+                <div className="space-y-[30px] !mt-[50px]">
+                  <div className="space-y-[20px]">
+                    <h1 className="text-4xl font-bold">
+                      Aditional Information
+                    </h1>
+                    <div className="h-[1px] w-full bg-[white]" />
+                    <table>
+                      <tbody className="text-white">
+                        <tr>
+                          <td>Rating</td>
+                          <td className="pl-5">
+                            : {data?.vote_average || "-"} / 10 from a total of{" "}
+                            {data?.vote_count || "-"} assessments
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Website Home Page</td>
+                          <td className="pl-5">: {data?.homepage || "-"}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+
+                <div className="space-y-[30px] !mt-[50px]">
+                  <div className="space-y-[20px]">
+                    <h1 className="text-4xl font-bold">Images</h1>
+                    <div className="h-[1px] w-full bg-[white]" />
+                    <div className="flex flex-col items-start justify-start">
+                      {dataImages?.backdrops
+                        ?.filter(
+                          ({ iso_639_1 }: { iso_639_1: string }) => !iso_639_1
+                        )
+                        .map(({ file_path }: { file_path: string }) => (
+                          <div>
+                            <img
+                              src={`${process.env.REACT_APP_IMAGE_URL}${file_path}`}
+                              alt="images"
+                            />
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-1">
+                  <Button
+                    onClick={() => {
+                      navigate("/");
+                    }}
+                    disabled={false}
+                  >
+                    Back To Home Page
+                  </Button>
                 </div>
               </div>
             </div>
-
-            <div className="grid grid-cols-1">
-              <Button
-                onClick={() => {
-                  navigate("/");
-                }}
-                disabled={false}
-              >
-                Back To Home Page
-              </Button>
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
       <Footer />
     </div>
